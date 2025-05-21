fe3d547ca0ace1afc68e8e1a59acfc685861d513
Pablo Jusim <pablo.jusim@gmail.com>
2025-05-21 17:02:13 -0300

    Agregado de modelos faltantes. Comienzo de modelo de ejemplares (copies)

---
+++ src/models/copy-model.js
@@ -0,0 +1,29 @@
+import { db } from "../config/db-connection.js";
+import { DataTypes } from "sequelize";
+
+export const CopyModel = db.define(
+    "Ejemplares",
+    {
+        id_ejemplar: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        id_libro: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        numero: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        estado: {
+            type: DataTypes.STRING(25),
+            allowNull: false
+        }
+  },
+  {
+    timestamps: true
+    }
+)
