cc343fc813df77ac39e648a88619b1aa62dfc694
Pablo Jusim <pablo.jusim@gmail.com>
2025-05-29 09:28:01 -0300

    Creación de middleware de autenticación de usuario Cambio en config/index para incluir clave de autenticación de usuario

---
+++ src/middlewares/check-user.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import CustomError from '../utils/custom-error.js';
+import config from '../config/index.js';
+
+export const checkTokenUser = (req, res, next) => {
+    try {
+        const tokenCookie = req.cookies.token;
+        if (!tokenCookie) throw new CustomError('Usuario no autorizado', 401);
+        const payload = jwt.verify(tokenCookie, config.SECRET_KEY_JWT);
+        req.user = payload;
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+};
